97ea573446ade8a5f097ec0f5bfd7e99e4635f67
itsthallis <thallis.s@hotmail.com>
2018-12-17 10:09:56 -0200

    Primeiro commit

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace C_
+{
+    class Class1
+    {
+        public string ReturnMessage()
+        {
+            return "Olá!!!";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c1 = new Class1();
+            Console.WriteLine($"Hello World! {c1.ReturnMessage()}");
+        }
+    }
+}
